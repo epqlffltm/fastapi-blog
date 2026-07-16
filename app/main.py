f9c6b6f19122dbd0f03d91758a277724b5d69116
epqlffltm <kjh55984249@gmail.com>
2026-07-16 17:14:47 +0900

    post api 생성

--- app/main.py
+++ app/main.py
@@ -5,13 +5,18 @@
 main.py작성
 
 2026-07-16
-get api 전체조회
+get api 전체 조회
+get api 단일 조회
+post api 생성
 '''
 
 from fastapi import FastAPI
+from pydantic  import BaseModel
+from datetime import datetime, timezone
 import random
 from .database.fake_posts import fake_posts
 from .database.fake_comments import fake_comments
+from .schemas import PostCreate, CommentCreate
 
 app = FastAPI()
 
@@ -40,12 +45,13 @@ def get_pages_handler(order: str = "random"):
         })
 
     if order == "asc":
-        return result
+        result.sort(key=lambda x: x["created_at"])
     elif order == "desc":
-        return result[::-1]
+        result.sort(key=lambda x: x["created_at"], reverse=True)
     else:
         random.shuffle(result)
-        return result
+
+    return result
 
 @app.get("/page/{id}")
 def get_page_handler(id: int):
@@ -57,3 +63,34 @@ def get_page_handler(id: int):
             comments.append(c)
 
     return {**post, "comments": comments}
+
+@app.post("/page")
+def create_post_handler(request: PostCreate):
+    new_id = max(fake_posts.keys()) + 1   # 서버가 id 발급
+    now = datetime.now(timezone.utc).isoformat()
+
+    fake_posts[new_id] = {
+        "id": new_id,
+        "created_at": now,
+        "updated_at": now,
+        "title": request.title,
+        "nickname": request.nickname,
+        "contents": request.contents,
+        "image": request.image,
+        "is_deleted": False,# 서버가 강제
+    }
+    return fake_posts[new_id]
+
+@app.post("/post/{id}")
+def create_comment_handler(id: int, request: CommentCreate):
+    new_id = max(fake_comments.keys()) + 1   # 댓글 고유 id 발급
+    now = datetime.now(timezone.utc).isoformat()
+
+    fake_comments[new_id] = {
+        "id": new_id,
+        "post_id": id,          # URL의 id = 어느 글에 다는지
+        "nickname": request.nickname,
+        "contents": request.contents,
+        "is_deleted": False,
+    }
+    return fake_comments[new_id]
---
+++ app/schemas.py
@@ -0,0 +1,19 @@
+#app/schemas.py
+
+'''
+2026-07-16
+schemas.py 작성
+'''
+
+from pydantic import BaseModel
+
+class ContentCreate(BaseModel):
+    nickname: str
+    contents: str
+
+class PostCreate(ContentCreate):
+    title: str
+    image: str
+
+class CommentCreate(ContentCreate):
+    post_id: int
